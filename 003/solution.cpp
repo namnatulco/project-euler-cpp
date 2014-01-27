55bcf6d9ec5e7491f998cf64ea6f4a7b54431a01
Rens van der Heijden <rens.vanderheijden@uni-ulm.de>
2014-01-28 00:05:48 +0100

    missing description, Makefile updated with a debug target, fixed several terrible bugs. Need sleep.

--- 003/solution.cpp
+++ 003/solution.cpp
@@ -9,8 +9,7 @@ typedef unsigned long long ull;
 
 const ull TARGET = 600851475143;
 
-
-vector<ull> primes; 
+vector<ull> primes;
 
 //this approach is very slow. There are many better ways, but for this question, it is
 //more than sufficient to "simply" brute-force the prime factors. In real scenarios, we
@@ -21,9 +20,15 @@ ull find_factor(ull number){
     primes.push_back(2);
     return (number/2);
   }else{
+    ull i;
+    if(primes.empty()){
+      i = 3;
+    } else {
+      i = primes.back();
+    }
     //rounded down due to integer division
     //we can increment by 2, since we're already sure that number is not even.
-    for(ull i=primes.back();i<(number/2);i=i+2){
+    for(;i<(number/2);i=i+2){
       if(number%i==0){
         //we found a new prime factor!
         primes.push_back(i);
@@ -39,16 +44,17 @@ int main(){
   ull intermediate=TARGET;
   // this condition is only useful when we happen to operate on powers of two.
   while(intermediate>1){
-    ull result = find_factor(TARGET);
+    ull result = find_factor(intermediate);
     if(result*2 > intermediate){
       primes.push_back(result);
       break; //this extra check saves us testing primes[primes.size()-2]..result again.
     }
+    intermediate = result;
   }
   cout << "Factorization of the number " << TARGET << " resulted in the primes {";
   for(auto prime : primes){
     cout << prime << ", ";
   }
-  cout << "}." << endl;
+  cout << "}. Therefore, the biggest prime factor of " << TARGET << " is " << primes.back() << endl;
   return 0;
 }
