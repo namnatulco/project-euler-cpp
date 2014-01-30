fa7319145343900f3256f0ceb3a51ebf2010e3a3
Rens van der Heijden <rens.vanderheijden@uni-ulm.de>
2014-01-30 23:49:18 +0100

    improved python solution

--- 004/solution.py
+++ 004/solution.py
@@ -1,9 +1,10 @@
 #Example brute-force solution in python
 
-l={}
-for i in range(999,99,-1):
-  for j in range(999,99,-1):
-    if str(i*j)==str(i*j)[::-1]:
-      l[i*j]=[i,j]
-l[max(l.keys())]
-
+l=[]
+m=0;
+for i in range(9999,999,-1):
+  for j in range(9999,999,-1):
+    if i*j>m and str(i*j)==str(i*j)[::-1] :
+      m=i*j
+      l=[i,j]
+print m, l
